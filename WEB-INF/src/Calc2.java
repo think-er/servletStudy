d37f44757a8c5b685b77592b4433daeddefea3bd
thinker <jeonghun5092@gmail.com>
2024-10-08 13:21:51 +0000

    Cookie 객체로 상태값 저장

--- WEB-INF/src/Calc2.java
+++ WEB-INF/src/Calc2.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,8 @@ public class Calc2 extends HttpServlet {
         ServletContext application = req.getServletContext();
         // Session
         HttpSession session = req.getSession();
+
+        Cookie[] cookies = req.getCookies();
 
         // UTF-8의 문자열 데이터를 보낸다.
         resp.setCharacterEncoding("UTF-8");
@@ -35,11 +38,25 @@ public class Calc2 extends HttpServlet {
         // 계산
         if(op.equals("=")) {
             // int x = (Integer)application.getAttribute("value");
-            int x = (Integer)session.getAttribute("value");
+            // int x = (Integer)session.getAttribute("value");
+            int x = 0;
+            for(Cookie c : cookies) {
+                if(c.getName().equals("value")) {
+                    x = Integer.parseInt(c.getValue());
+                    break;
+                }
+            }
             int y = v;
             // String operator = (String)application.getAttribute("op");
-            String operator = (String)session.getAttribute("op");
+            // String operator = (String)session.getAttribute("op");
             
+            String operator = "";
+            for(Cookie c : cookies) {
+                if(c.getName().equals("op")) {
+                    operator = c.getValue();
+                    break;
+                }
+            }
             if(operator.equals("+"))
                 result = x + y;
             else
@@ -51,8 +68,14 @@ public class Calc2 extends HttpServlet {
         else {
             // application.setAttribute("value", v);
             // application.setAttribute("op", op);
-            session.setAttribute("value", v);
-            session.setAttribute("op", op);
+            // session.setAttribute("value", v);
+            // session.setAttribute("op", op);
+
+            Cookie valueCookie = new Cookie("value", String.valueOf(v));
+            Cookie opCookie = new Cookie("op", op);
+            resp.addCookie(valueCookie);
+            resp.addCookie(opCookie);
+
             req.getRequestDispatcher("/calc2.jsp").forward(req, resp);            
         }
 
